5a79e079473d25f1b241ca1e8f37a71e30a19734
prakharsethi22 <prakhar.sethi@outlook.com>
2023-12-21 02:56:06 +0530

    finalized main

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f'{num_words} found in the text')
+    freqMap = count_letters(text)
+    sorted_freqMap = sort_count_map(freqMap)
+    for item in sorted_freqMap:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
     
 def get_num_words(text):
     return len(text.split())
@@ -11,4 +18,23 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
     
+def count_letters(text):
+    freqMap = {}
+    for c in text:
+        ch = c.lower()
+        if ch in freqMap:
+            freqMap[ch] += 1
+        else:
+            freqMap[ch] = 1
+    return freqMap
+
+def sort_on(d):
+    return d["num"]
+        
+def sort_count_map(freqMap):
+    sorted_list = []
+    for ch in freqMap:
+        sorted_list.append({"char" : ch, "num" : freqMap[ch]})
+    sorted_list.sort(reverse=True,key=sort_on)
+    return sorted_list    
 main()
