957c932bf191488042148c1afe319eb1d2b1c997
prakharsethi22 <prakhar.sethi@outlook.com>
2023-12-21 02:39:40 +0530

    update main

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    book_path = "books/text.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f'{num_words} found in the text')
+    
+def get_num_words(text):
+    return len(text.split())
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+main()
